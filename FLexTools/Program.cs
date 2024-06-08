03d6f1ad009419ebd23903d46cb3949932a5c41f
FL <felip3.fl@gmail.com>
2024-06-08 17:35:36 -0300

    First commit

---
+++ FLexTools/Program.cs
@@ -0,0 +1,33 @@
+﻿using DevTools;
+
+namespace FLexTools
+{
+    class TestClass
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var parameters = args.FirstOrDefault().ToUpper();
+
+            switch (parameters)
+            {
+                case "CPF":
+                    GerarCpf();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void GerarCpf()
+        {
+            var cpf = new CPF().GerarCpf();
+            Console.WriteLine(cpf);
+        }
+
+    }
+
+
+}
+
+
