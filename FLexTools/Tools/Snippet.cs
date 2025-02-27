2383c89742fba6aea6da97eea1b099ea27dcb718
Felip3FL <felip3.fl@gmail.com>
2025-02-26 22:29:24 -0300

    New method Snippet

---
+++ FLexTools/Tools/Snippet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLexTools.Tools
+{
+    public class Snippet
+    {
+        public string ConvertCodeToVisualStudioCodeSnippet(string code)
+        {
+            var result = code;
+            var result2 = result.Replace("\t", "");
+            var result3 = result2.Replace("\n", "\",\nAAA");
+
+            return result3;
+        }
+    }
+}
